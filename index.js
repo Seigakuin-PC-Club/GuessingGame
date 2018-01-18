120450414fd8c44b6f03e850a1f6fdfb3ebd4d80
S Fujimoto <unicoshun@gmail.com>
2018-01-18 09:10:05 +0900

    change in index.js

--- index.js
+++ index.js
@@ -6,10 +6,9 @@
 
 // write function to remove element
 
-
 // set Random Number to guess 1 - 100
 // use Math.random() / Math.floor()
 
 // set setTimeout for outputDOM
 
-
+// comment added to master
